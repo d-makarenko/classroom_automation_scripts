ec40bbf27dd9d9b228dace99bdd393aaf8e8c7cd
dmitry <dmtr.makarenko@gmail.com>
2020-10-25 13:17:27 +0300

    Added repo cloning function

--- github_print_ready_to_check_homeworks.py
+++ github_print_ready_to_check_homeworks.py
@@ -1,5 +1,7 @@
 from github import Github
+from git import Repo
 import re
+import os
 from classroom_automation_tokens import GITHUB_TOKEN
 from tqdm import tqdm
 
@@ -32,14 +34,28 @@ class Group(object):
 
         return True
     
+
+    def get_login(self, repo):
+        _, login = re.match(".*-no(\d)-(.*)", repo.name).groups()
+        return login
+
+    def get_dz(self, repo):
+        dz, _ = re.match(".*-no(\d)-(.*)", repo.name).groups()
+        return dz
+
+    def get_name(self, repo):
+        name = self.ln[self.get_login(repo)]
+        return name
+
+    def get_variant(self, repo):
+        return list(self.ln.keys()).index(self.get_login(repo)) + 1
+
     def format_repo(self, repo, grades=False):
         
         dz, login = re.match(".*-no(\d)-(.*)", repo.name).groups()
         name = self.ln[login]
         variant = list(self.ln.keys()).index(login) + 1
         if(grades):
-            if("loponavt" in repo.name):
-                pass
             pull = list(filter(lambda x: x.title=="Feedback", repo.get_pulls(state="all")))[0]
             
             grade = 0
@@ -49,11 +65,11 @@ class Group(object):
                     break
             return f"\n {variant} {name} \n" + f"\t https://github.com/MIEE-ACS/{repo.name}/pull/1\n\tОценка: {grade}\n"
         else:
-            return f"\n {variant} {name} \n" + f"\t https://github.com/MIEE-ACS/{repo.name}/pull/1"
+            return f"\n {variant} {name} \n" + f"\t https://github.com/MIEE-ACS/{repo.name}"
 
-    def get_dz(repo):
-        return re.match(".*-no(\d)-.*", repo.name).groups()[0]
 
+    def get_not_graded_repos(self, dz="*"):
+        return [repo for repo in self.not_merged if dz=="*" or dz==get_dz(repo)]
 
     def get_repos_to_check(self, dz="*"):
         return [self.format_repo(repo) for repo in self.not_merged if dz=="*" or dz==get_dz(repo)]
@@ -149,3 +165,36 @@ for group in groups:
 for group in groups:
     print(f"\n{group.name}")
     print("\n".join(group.get_graded_repo()))
+
+
+download_repos = input("Do you want to clone\pull not graded repos? ([Y]es/No)")
+if(download_repos.lower() in ["", "yes", "y", "да", "д", "нуы", "н"]):
+    default_path = os.path.realpath(os.curdir)
+    directory_to_clone = ""
+    while not os.path.exists(directory_to_clone):
+        directory_to_clone = input(f"Directory to clone repos [{default_path}]: ")
+        if not directory_to_clone:
+            directory_to_clone = default_path
+        if not os.path.exists(directory_to_clone):
+            create_dir = input(f"Path {directory_to_clone} do not exists. Create now? [Y]es/ No: ")
+            if create_dir.lower() in ["", "yes", "y", "да", "д", "нуы", "н"]:
+                os.makedirs(directory_to_clone)
+    
+
+
+    group_progress = tqdm(groups, total=len(groups), desc="Cloning repositories")
+    for group in group_progress:
+        group_dir = os.path.join(directory_to_clone, group.name)
+        os.makedirs(group_dir, exist_ok=True)
+        repos = group.get_not_graded_repos()
+        repo_progress = tqdm(repos, total=len(groups), desc=f"Cloning repositories: {group.name}")
+        for repo in repo_progress:
+            repo_dir = os.path.join(group_dir, group.get_dz(repo), f"{group.get_variant(repo):02} - {group.get_name(repo)}")
+            if os.path.exists(repo_dir):
+                repo = Repo(repo_dir)
+                repo.head.reset(index=True, working_tree=True)
+                repo.remote().pull()
+            else:
+                os.makedirs(repo_dir, exist_ok=True)
+                Repo.clone_from(f"https://github.com/MIEE-ACS/{repo.name}", repo_dir)
+            
